274d608503e4dfe1e2e3ed9bd6890eaffa66a1d1
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-11-15 18:49:26 +0400

    Redux Thunk Task

--- src/store/customerReduser.js
+++ src/store/customerReduser.js
@@ -1,12 +1,15 @@
-const ADD_MANY_CUSTOMERS = 'ADD_MANY_CUSTOMERS';
+const ADD_MANY_CUSTOMERS = "ADD_MANY_CUSTOMERS";
 
-export const customerReducer = (state = [{name:'Антон'}] , action) =>{
-    switch(action.type){
-        case ADD_MANY_CUSTOMERS:
-            return [...state, ...action.payload]
-        default:
-            return state
-    }
-}
+export const customerReducer = (state = [{ name: "Антон" }], action) => {
+  switch (action.type) {
+    case ADD_MANY_CUSTOMERS:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
 
-export const addManyCustomersAction = (payload) => ({type: ADD_MANY_CUSTOMERS, payload})
+export const addManyCustomersAction = (payload) => ({
+  type: ADD_MANY_CUSTOMERS,
+  payload,
+});
--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,5 @@ import { applyMiddleware, createStore } from "redux";
 import { customerReducer } from "./customerReduser";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(customerReducer)
+// Вторым аргументом назначить MIDDLEWARE/THUNK.
+export const store = createStore(customerReducer, applyMiddleware(thunk));
